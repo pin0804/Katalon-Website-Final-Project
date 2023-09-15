0c9e01572f365892da58562707164698d112f575
agfina <agfinaa@gmail.com>
2023-09-15 13:24:41 +0700

    delete unuse objects, add tc send form contach us

---
+++ Scripts/TC Homepage/TC_Send form Contact Us/Script1694759038778.groovy
@@ -0,0 +1,33 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser(GlobalVariable.Browser)
+
+WebUI.waitForElementVisible(findTestObject('Homepage/Our Products/div_HEADPHONES'), 0)
+
+WebUI.click(findTestObject('Homepage/Menu Bar/btn_CONTACT US'))
+
+WebUI.delay(5, FailureHandling.STOP_ON_FAILURE)
+
+WebUI.callTestCase(findTestCase('Reusable Test/TC_RT_Homepage/TC_Verify element Contact Us'), [:], FailureHandling.STOP_ON_FAILURE)
+
+WebUI.verifyElementNotClickable(findTestObject('Homepage/Contact Us/txt_SEND'))
+
+WebUI.click(findTestObject(null))
+
